ced1858030f64b7b8644da5c2e27e84a9cca41d4
jprctr <mail@jamesaproctor.com>
2024-08-20 16:06:36 -0400

    trying out a css3d solution; effect likely to be compromised

--- inertia.js
+++ inertia.js
@@ -1,25 +1,67 @@
 import * as THREE from 'three';
+import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 
-export function SetupScene() {
+export function SetupScene(containerId, itemsId) {
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const container = document.getElementById(containerId);
+  // const camera = new THREE.PerspectiveCamera( 75, container.offsetWidth / container.offsetHeight, 0.1, 1000 );
+  const camera = new THREE.OrthographicCamera( container.offsetWidth / - 2, container.offsetWidth / 2, container.offsetHeight / 2, container.offsetHeight / - 2, 1, 1000 );
+  
+  const renderer = new CSS3DRenderer();
+  renderer.setSize( container.offsetWidth, container.offsetHeight );
+  container.appendChild( renderer.domElement );
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  // grab the children of the target, position and add to scene
+  const items = document.getElementById(itemsId).children;
+  const cards = [];
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  // let itemIndex = 0;
+  const padding = 20; // idk 
+  let itemOffset = padding;
+  let maxItemHeight = 0;
+  for (const item of items) {
+    const itemCSSObject = new CSS3DObject( item );
 
-  camera.position.z = 5;
+    // we may need to recalculate these metrics on resize or just reset the whole thing
+    itemCSSObject.userData = {
+      initX: itemOffset + (item.offsetWidth / 2),
+      width: item.offsetWidth,
+    };
+    itemCSSObject.position.x = itemCSSObject.userData.initX; // itemOffset + (item.offsetWidth / 2);
+    itemCSSObject.position.y = 0;
+    itemCSSObject.position.z = 0;
+
+    cards.push(itemCSSObject);
+    scene.add(itemCSSObject);
+
+    itemOffset += item.offsetWidth + padding;
+    maxItemHeight = Math.max(maxItemHeight, item.offsetHeight);
+  }
+
+  // camera.position.z = 480; // 640; // magic numbers... not needed now w/ orthographic
 
   function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cards.forEach((c, i) => {
+      c.position.x -= 2; // vary this value to change direction, intensity
+
+      // clean this up and also check for the opposite direction
+      if (c.position.x + c.userData.width / 2 + container.offsetWidth / 2 < 0) {
+        c.position.x = c.userData.width / 2 + Math.max(...cards.map(ci => ci.position.x + ci.userData.width / 2 + padding)); // c.userData.initX + container.offsetWidth / 2;
+      }
+    });
+
     renderer.render( scene, camera );
+    requestAnimationFrame( animate );
   }
-  
-  renderer.setAnimationLoop( animate );
+
+  animate();
+
+  function onWindowResize() {
+    camera.aspect = container.offsetWidth / container.offsetHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( container.offsetWidth, container.offsetHeight );
+  }
+
+  window.addEventListener( 'resize', onWindowResize );
+
 }
