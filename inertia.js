a5720c68972cb9bd94c1c011569dafd13c7f4ba9
jprctr <mail@jamesaproctor.com>
2024-08-23 16:38:21 -0400

    add title and description to cards

--- inertia.js
+++ inertia.js
@@ -19,6 +19,86 @@ const speeds = {
   tolerance: 0.001, // smallest value we care about
 };
 
+const textStyles = `
+  .inertia-text-container {
+    font-family: sans-serif;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    font-size: 1rem;
+  }
+  .inertia-text {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    margin-top: auto;
+    padding: 3em 4em;
+    max-width: 48em;
+    min-height: 12em;
+    gap: 1.5em;
+    color: white;
+    text-shadow: 0 0 0.5em black;
+  }
+  .inertia-text .inertia-title span {
+    font-size: 2.75em;
+    font-weight: 600;
+  }
+  .inertia-text .inertia-description span {
+    font-size: 2.25em;
+    line-height: 1.5em;
+    font-weight: 400;
+  }
+`;
+
+function generateTextTexture(slide, width, height) {
+  return new Promise((resolve) => {
+    const { title, description } = slide;
+
+    const text = `
+      <div class="inertia-text">
+        <div class="inertia-title">
+          <span>
+            ${title}
+          </span>
+        </div>
+        <div class="inertia-description">
+          <span>
+            ${description}
+          </span>
+        </div>
+      </div>
+    `;
+
+    // create canvas
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+
+    // insert text into canvas
+    const svg = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="${canvas.width}" height="${canvas.height}">
+        <style>${textStyles}</style>
+        <foreignObject width="100%" height="100%">
+          <div class="inertia-text-container" xmlns="http://www.w3.org/1999/xhtml">${text}</div>
+        </foreignObject>
+      </svg>
+    `;
+    const blob = new Blob([svg], { type: 'image/svg+xml;charset=utf-8' });
+    const url = URL.createObjectURL(blob);
+    const image = new Image();
+
+    // draw to canvas, create texture from canvas
+    image.onload = () => {
+      context.drawImage(image, 0, 0);
+      URL.revokeObjectURL(url);
+      // return canvas as texture
+      return resolve(new THREE.CanvasTexture(canvas));
+    }
+    image.src = url;
+  });
+}
+
 export async function SetupScene(containerId, slides) {
   // Setup
 
@@ -45,58 +125,74 @@ export async function SetupScene(containerId, slides) {
   shaderPass.uniforms['aspect'].value = container.offsetWidth / container.offsetHeight;
   composer.addPass(shaderPass);
 
-  // Construct Objects
+  // Construct Objects & assign positions
 
-  const meshes = await Promise.all(
+  const cards = await Promise.all(
     slides.map((slide) => (
       new Promise(resolve => (
+        // load the image as a texture
         textureLoader.load(slide.image, (texture) => {
+          // create card group and save shared properties
+          const card = new THREE.Group();
           const aspect = texture.image.width / texture.image.height;
-
           const geometry = new THREE.PlaneGeometry(aspect, 1, 1, 1);
+          card.userData = {
+            ...slide,
+            aspect,
+          };
 
+          // create image mesh
           const uniforms = {
             map: { type: 't', value: texture },
             radius: { type: 'f', value: radius },
             aspect: { type: 'f', value: aspect },
           };
-          const material = new THREE.ShaderMaterial({
+          const imageMaterial = new THREE.ShaderMaterial({
             uniforms,
             vertexShader,
             fragmentShader,
           });
+          const imageMesh = new THREE.Mesh(geometry, imageMaterial);
+          card.add(imageMesh);
 
-          const mesh = new THREE.Mesh(geometry, material);
-          mesh.userData = {
-            ...slide,
-            aspect,
-          };
+          const textHeight = 1024; // use the same height to keep text scale consistent
+          const textWidth = textHeight * aspect; // match image aspect ratio
 
-          return resolve(mesh);
+          // generate text texture w/ matching dimensions
+          generateTextTexture(slide, textWidth, textHeight).then((textTexture) => {
+            // create text mesh
+            const textMaterial = new THREE.MeshBasicMaterial({ map: textTexture, transparent: true, });
+            const textMesh = new THREE.Mesh(geometry, textMaterial);
+            card.add(textMesh);
+
+            // return the assembled card
+            return resolve(card);
+          });
         })
       ))
     ))
   );
 
+  // assign positions
   let xOffset = 0;
-  meshes.forEach(mesh => {
-    const width = mesh.userData.aspect;
-    mesh.userData.xOffset = xOffset + width / 2;
+  cards.forEach(card => {
+    const width = card.userData.aspect;
+    card.userData.xOffset = xOffset + width / 2;
     xOffset += width + margin;
-    scene.add(mesh);
+    scene.add(card);
   });
 
   // Render
 
-  const fullWidth = meshes.map(c => (c.userData.aspect + margin)).reduce((a, v) => a + v, 0);
+  const fullWidth = cards.map(card => (card.userData.aspect + margin)).reduce((a, v) => a + v, 0);
   const halfWidth = fullWidth * 0.5;
   const bigOffset = 100000 * fullWidth; // helps make it "infinite" in either direction
 
   let speed = speeds.stopped;
   let currentOffset = 0;
   function animate() {
-    meshes.forEach(mesh => {
-      mesh.position.x = ((mesh.userData.xOffset + currentOffset + bigOffset) % fullWidth) - halfWidth;
+    cards.forEach(card => {
+      card.position.x = ((card.userData.xOffset + currentOffset + bigOffset) % fullWidth) - halfWidth;
     });
 
     const effectiveSpeed = (speed || speeds.default); // default to very slow scroll if no input
@@ -173,14 +269,15 @@ export async function SetupScene(containerId, slides) {
       (((clientY - offsetTop) / offsetHeight) * 2 - 1) * -1,
     );
     raycaster.setFromCamera(pointer, camera);
-    const [intersect] = raycaster.intersectObjects(scene.children, false);
+    const [intersect] = raycaster.intersectObjects(scene.children);
     if (intersect) {
       const { object } = intersect;
-      const { userData } = object;
+      const { parent } = object;
+      const { userData } = parent;
       const { link } = userData;
       if (link) {
         window.open(link); // new tab is nicer for dev
-        // window.open(link, '_self'); // maybe link directly in this tab later
+        // window.open(link, '_self'); // link directly in this tab later
       }
     }
   }
