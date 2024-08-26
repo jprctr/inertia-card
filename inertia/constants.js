543b04dd1631579bda46cfe72b8d837d90b13246
jprctr <mail@jamesaproctor.com>
2024-08-26 10:57:51 -0400

    tone down speeds a bit (keys, drag)

--- inertia/constants.js
+++ inertia/constants.js
@@ -5,10 +5,10 @@ export const radius = 0.05; // border radius for images expressed as % of height
 export const speeds = {
   stopped: 0,
   default: -0.001,
-  arrowLeft: 0.1,
-  arrowRight: -0.1,
+  arrowLeft: 0.05,
+  arrowRight: -0.05,
+  dragMod: 0.2,
   wheelMod: 0.001,
-  dragMod: 0.5,
   tolerance: 0.001, // smallest value we care about
 };
 
